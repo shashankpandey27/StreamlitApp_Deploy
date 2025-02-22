3ce57e92409f3977baf1cd83cf917a1c1c0eea01
Pandey <shashank.i.pandey@capgemini.com>
2025-02-22 13:06:01 +0400

    Updated Streamlit app

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Streamlit App on Windows with GitHub Actions")
 st.write("This app is running as a Windows Scheduled Task!")
 
-st.write("Updated: CI/CD without SSH on Windows!")
+#st.write("Updated: CI/CD without SSH on Windows!")
